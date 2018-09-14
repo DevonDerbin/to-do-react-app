b4fedf933dcf25c7c1c44a0189f3f8ee313f000e
Andy Rankin <wave@andrewdrankin.com>
2018-09-14 14:35:19 +0100

    button fiddler

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -9,21 +9,24 @@ class ListItem extends Component {
             completed: this.props.completed
         }
         this.handleDetailToggle = this.handleDetailToggle.bind(this);
+        // this.handleCompleteToggle = this.handleCompleteToggle.bind(this);
     }
-
     handleDetailToggle() {
         this.setState({
             detailsHidden: !this.state.detailsHidden,
-            completed: !this.state.completed
+            completed: !this.props.completed
         })
     }
-
+    // handleCompleteToggle() {
+    //     this.setState({
+    //     })
+    // }
     render () {
         let {id, title, notes, dueDate, completed, onClick, onDelete} = this.props;
         return (
             <li key={ id } className="list-group-item">
                 <div className="card-header">
-                    <button className={"btn " + (this.state.completed ? "btn-success" : "btn-light")} onClick={ onClick }>complete</button>
+                    <button className={"btn " + this.props.completed ? "btn-warning" : "btn-light"} onClick={ onClick }>complete</button>
                     <span className="card-title">{title}</span>
                     <button className="btn btn-dark" onClick={ this.handleDetailToggle }>expand</button>
                 </div>
